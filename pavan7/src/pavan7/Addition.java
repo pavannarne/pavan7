bb0ae367c7fa7ef8264d4954899a091aa28b6596
pavannarne <pavannarne@gmail.com>
2014-06-10 11:37:20 -0700

    second commit

---
+++ pavan7/src/pavan7/Addition.java
@@ -0,0 +1,30 @@
+package pavan7;
+
+public class Addition 
+
+{
+	
+	public static void main(String[] args) 
+	
+	{
+
+		   int i = 2; int j = 3;
+		   
+		   int sum= sum(i,j);
+		   System.out.println("The value of i is :"+i);
+		   System.out.println("The value of j is :" +j);
+		   
+		   System.out.println("The sum of i and j is:"+sum);
+
+		   
+		  }
+
+		  public static int sum(int i, int j) {
+
+		  
+		  return i+j;
+		  }
+
+		}
+
+
